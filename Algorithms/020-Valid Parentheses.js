3be15a1a3f6e2cb5d0859753d04760bad615d443
CinGing <chenguo307@163.com>
2018-09-01 23:42:13 +0800

    add

---
+++ Algorithms/020-Valid Parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var map = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+}
+var isValid = function(s) {
+    var stack = [];
+    for (var i = 0; i < s.length; i++) {
+        if (map[s[i]])
+            stack.push(map[s[i]]);
+        else if (s[i] !== stack.pop()) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
+
+a = "(([[{{}]]))"
+console.log(isValid(a));
