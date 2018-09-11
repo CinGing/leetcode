af6e0972614bbf1a7d8ee9778b9d0245896a3910
CinGing <chenguo307@163.com>
2018-09-11 23:50:43 +0800

    add

---
+++ Algorithms/064-Minimum Path Sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    var m = grid.length; // row
+    var n = grid[0].length; // column
+    for (var i = 0; i < m; i++) {
+        for (var j = 0; j < n; j++) {
+            if (i == 0 && j != 0) {
+                grid[i][j] = grid[i][j] + grid[i][j - 1];
+            } else if (i != 0 && j == 0) {
+                grid[i][j] = grid[i][j] + grid[i - 1][j];
+            } else if (i == 0 && j == 0) {
+                grid[i][j] = grid[i][j];
+            } else {
+                grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) +
+                    grid[i][j];
+            }
+        }
+    }
+    return grid[m - 1][n - 1];
+};
+var a = [
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+];
+console.log(minPathSum(a));
